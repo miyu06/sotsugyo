c22b77104f2d6366fdd018a3ee93410083ac9c51
Nagomi Totsuka <cl_s3226541078@soshi.jp>
2024-05-16 14:39:20 +0900

    a

--- sotsugyo/ViewController.swift
+++ sotsugyo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
-
+    @IBOutlet var label2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
